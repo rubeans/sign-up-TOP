91aa686162d616a10af5f384044dd8b7b97975ef
rubeans <rubensglf@gmail.com>
2022-12-06 20:01:00 -0300

    check passwords

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-const password = document.querySelector('#password')
-const confirmPassword = document.querySelector('#confirm-password')
+const checkPassword = () => {
+    let password = document.querySelector('#password')
+    let confirmPassword = document.querySelector('#confirm-password')
+    let button = document.querySelector('button')
+    let errorMsg = document.querySelector('.error-msg')
+
+
+    if (password.value == confirmPassword.value) {
+        console.log('Passed')
+    }
+    else {
+        console.log('Didnt pass')
+        confirmPassword.style.borderColor = 'red'
+        confirmPassword.style.backgroundColor = 'red'
+        confirmPassword.style.color = '#fff'
+        errorMsg.textContent = "Didn't match! Please try again."
+        button.disabled = true
+    }
+}
