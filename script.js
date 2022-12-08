5112afa850c8b1eca798953fb400bae5dea905cd
rubeans <rubensglf@gmail.com>
2022-12-07 23:01:00 -0300

    change borders to red when password is red

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ const checkPassword = () => {
     let errorMsg = document.querySelector('.error-msg')
 
 
-    if (password.value == confirmPassword.value) {
-        console.log('Passed')
-    }
-    else {
-        console.log('Didnt pass')
-        confirmPassword.style.borderColor = 'red'
-        confirmPassword.style.backgroundColor = 'red'
-        confirmPassword.style.color = '#fff'
-        errorMsg.textContent = "Didn't match! Please try again."
-        button.disabled = true
+    if (password.value.length != 0) {
+        if (password.value == confirmPassword.value) {
+            console.log('Passed')
+        }
+        else {
+            console.log('Didnt pass')
+            confirmPassword.style.borderColor = 'red'
+            password.style.borderColor = 'red'
+            errorMsg.textContent = "The passwords didn't match!"
+        }
     }
 }
